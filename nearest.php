0edb815e80f0b3be4b73fc0839bddb08c48ff2cd
Michał Żuk <michal.zuk30601@gmail.com>
2023-03-18 22:52:19 +0100

    najblizsze spotkania

---
+++ nearest.php
@@ -0,0 +1,150 @@
+<!DOCTYPE html>
+<html lang="pl">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Kalendarz</title>
+    <link rel="stylesheet" href="style/style.css" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
+</head>
+
+<body>
+    <div class="container">
+        <main class="main">
+            <header class="header">
+                <a href="index.php" class="header_link">
+                    <h1>Kalendarz</h1>
+                </a>
+            </header>
+            <section>
+                <header>
+                    <h2>Najbliższe spotkania</h2>
+                </header>
+                <div class="meetings_n">
+                    <div class="meeting_n">
+                        <article>
+                            <header>
+                                <h3>Tytuł spotkania</h3>
+                                <p>
+                                    Dzień: 03.05.2023 r. <br />
+                                    Godzina: 12:30
+                                </p>
+                            </header>
+                        </article>
+                    </div>
+                    <div class="meeting_n">
+                        <article>
+                            <header>
+                                <h3>Tytuł spotkania</h3>
+                                <p>
+                                    Dzień: 03.05.2023 r. <br />
+                                    Godzina: 12:30
+                                </p>
+                            </header>
+                        </article>
+                    </div>
+                    <div class="meeting_n">
+                        <article>
+                            <header>
+                                <h3>Tytuł spotkania</h3>
+                                <p>
+                                    Dzień: 03.05.2023 r. <br />
+                                    Godzina: 12:30
+                                </p>
+                            </header>
+                        </article>
+                    </div>
+                    <div class="meeting_n">
+                        <article>
+                            <header>
+                                <h3>Tytuł spotkania</h3>
+                                <p>
+                                    Dzień: 03.05.2023 r. <br />
+                                    Godzina: 12:30
+                                </p>
+                            </header>
+                        </article>
+                    </div>
+                    <div class="meeting_n">
+                        <article>
+                            <header>
+                                <h3>Tytuł spotkania</h3>
+                                <p>
+                                    Dzień: 03.05.2023 r. <br />
+                                    Godzina: 12:30
+                                </p>
+                            </header>
+                        </article>
+                    </div>
+                    <div class="meeting_n">
+                        <article>
+                            <header>
+                                <h3>Tytuł spotkania</h3>
+                                <p>
+                                    Dzień: 03.05.2023 r. <br />
+                                    Godzina: 12:30
+                                </p>
+                            </header>
+                        </article>
+                    </div>
+                    <div class="meeting_n">
+                        <article>
+                            <header>
+                                <h3>Tytuł spotkania</h3>
+                                <p>
+                                    Dzień: 03.05.2023 r. <br />
+                                    Godzina: 12:30
+                                </p>
+                            </header>
+                        </article>
+                    </div>
+                    <div class="meeting_n">
+                        <article>
+                            <header>
+                                <h3>Tytuł spotkania</h3>
+                                <p>
+                                    Dzień: 03.05.2023 r. <br />
+                                    Godzina: 12:30
+                                </p>
+                            </header>
+                        </article>
+                        <div class="meeting_n">
+                            <article>
+                                <header>
+                                    <h3>Tytuł spotkania</h3>
+                                    <p>
+                                        Dzień: 03.05.2023 r. <br />
+                                        Godzina: 12:30
+                                    </p>
+                                </header>
+                            </article>
+                        </div>
+                    </div>
+            </section>
+        </main>
+        <nav class="nav">
+            <!-- grafika z https://makersacademy.co.za/tpl-2_31_29-.html -->
+            <div class="change">
+                <a class="profile"><img src="img/profile.png" alt="zdjęcie profilowe" class="profile_img" /></a>
+            </div>
+            <div class="options">
+                <a href="profile.php" class="option">Mój profil</a>
+                <a href="nearest.php" class="option">Najbliższe spotkania</a>
+                <a class="option" href="friends.php">Moi znajomi</a>
+                <a class="option" href="logout.php">Wyloguj</a>
+            </div>
+        </nav>
+    </div>
+
+    <footer class="footer">
+        <p>
+            Strona wykonana przez <a href="https://github.com/michasGH30?tab=repositories" target="_blank">Michał Żuk</a> &copy; 2023.
+        </p>
+    </footer>
+    <script src="scripts/script.js"></script>
+    <script src="scripts/scroll.js"></script>
+</body>
+
+</html>
--- scripts/scroll.js
+++ scripts/scroll.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
     let w = $(window).height();
     let f = $(".friends_list").height();
     let u = $(".users").height();
+    let m = $(".meetings_n").height();
     let vh = f / w;
     // alert(vh);
     if (vh >= 0.4499) {
@@ -11,5 +12,10 @@ $(document).ready(function () {
     // alert(vh);
     if (vh >= 0.44999) {
         $(".users").css("overflow-y", "scroll");
+    }
+    vh = m / w;
+    // alert(vh);
+    if (vh >= 0.44998) {
+        $(".meetings_n").css("overflow-y", "scroll");
     }
 });
